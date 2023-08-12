c8f83f1a1fffce85e7e9954f04ccecd8d62a7f58
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 14:51:24 -0300

    test(account): add the account validations tests

--- domain/src/test/java/com/kaua/ecommerce/users/domain/accounts/AccountTest.java
+++ domain/src/test/java/com/kaua/ecommerce/users/domain/accounts/AccountTest.java
@@ -1,5 +1,7 @@
 package com.kaua.ecommerce.users.domain.accounts;
 
+import com.kaua.ecommerce.users.domain.exceptions.DomainException;
+import com.kaua.ecommerce.users.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +33,149 @@ public class AccountTest {
         Assertions.assertNull(aAccount.getAvatarUrl());
         Assertions.assertNotNull(aAccount.getCreatedAt());
         Assertions.assertNotNull(aAccount.getUpdatedAt());
+    }
+
+    @Test
+    public void givenAnInvalidFirstName_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "";
+        final var aLastName = "Pereira";
+        final var aEmail = "teste@teste.com";
+        final var aPassword = "123456Ab";
+        final var expectedErrorMessage = "'firstName' should not be null or blank";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidLastName_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "Kaua";
+        final String aLastName = null;
+        final var aEmail = "teste@teste.com";
+        final var aPassword = "123456Ab";
+        final var expectedErrorMessage = "'lastName' should not be null or blank";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidEmail_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "Kaua";
+        final var aLastName = "Pereira";
+        final var aEmail = "";
+        final var aPassword = "123456Ab";
+        final var expectedErrorMessage = "'email' should not be null or blank";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidPassword_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "Kaua";
+        final var aLastName = "Pereira";
+        final var aEmail = "teste@teste.com";
+        final String aPassword = null;
+        final var expectedErrorMessage = "'password' should not be null or blank";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidPasswordLength_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "Kaua";
+        final var aLastName = "Pereira";
+        final var aEmail = "teste@teste.com";
+        final var aPassword = "1234";
+        final var expectedErrorMessage = "'password' should be at least 8 characters";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidPasswordButNotContainsLowerAndUpercaseLetter_whenCallsNewAccount_thenAnExceptionShouldBeThrown() {
+        // given
+        final var aFirstName = "Kaua";
+        final var aLastName = "Pereira";
+        final var aEmail = "teste@teste.com";
+        final var aPassword = "12345678";
+        final var expectedErrorMessage = "'password' should contain at least one uppercase letter, one lowercase letter and one number";
+
+        // when
+        final var aAccount = Account.newAccount(
+                aFirstName,
+                aLastName,
+                aEmail,
+                aPassword
+        );
+
+        final var aException = Assertions.assertThrows(DomainException.class, () -> aAccount
+                .validate(new ThrowsValidationHandler()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
     }
 }
