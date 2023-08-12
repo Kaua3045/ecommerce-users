7354cfd6a339081ee4a8a3fe1465438a36f45828
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 15:14:59 -0300

    feat(exception): add the NoStackTraceException and refactor DomainException to use NoStackTraceException

--- domain/src/main/java/com/kaua/ecommerce/users/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/exceptions/DomainException.java
@@ -4,13 +4,17 @@ import com.kaua.ecommerce.users.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStackTraceException {
 
     private final List<Error> errors;
 
     private DomainException(final List<Error> aErrors) {
-        super("", null, true, false);
+        super("");
         this.errors = aErrors;
+    }
+
+    public static DomainException with(final Error aError) {
+        return new DomainException(List.of(aError));
     }
 
     public static DomainException with(final List<Error> aErrors) {
---
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/exceptions/NoStackTraceException.java
@@ -0,0 +1,12 @@
+package com.kaua.ecommerce.users.domain.exceptions;
+
+public class NoStackTraceException extends RuntimeException {
+
+    public NoStackTraceException(final String message) {
+        this(message, null);
+    }
+
+    public NoStackTraceException(final String message, final Throwable cause) {
+        super(message, cause, true, false);
+    }
+}
