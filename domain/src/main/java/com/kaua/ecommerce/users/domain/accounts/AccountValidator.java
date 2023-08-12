cf445a0d03ff64dc58e1378ebe33833991d49671
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 14:35:42 -0300

    feat(account): add the validations in account validator class

--- domain/src/main/java/com/kaua/ecommerce/users/domain/accounts/AccountValidator.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/accounts/AccountValidator.java
@@ -1,11 +1,13 @@
 package com.kaua.ecommerce.users.domain.accounts;
 
+import com.kaua.ecommerce.users.domain.validation.Error;
 import com.kaua.ecommerce.users.domain.validation.ValidationHandler;
 import com.kaua.ecommerce.users.domain.validation.Validator;
 
 public class AccountValidator extends Validator {
 
     private final Account account;
+    private final int PASSWORD_MINIMUM_LENGTH = 8;
 
     public AccountValidator(final Account aAccount, final ValidationHandler aHandler) {
         super(aHandler);
@@ -14,6 +16,24 @@ public class AccountValidator extends Validator {
 
     @Override
     public void validate() {
+        if (account.getFirstName() == null || account.getFirstName().isBlank()) {
+            this.validationHandler().append(new Error("'firstName' should not be null or blank"));
+        }
 
+        if (account.getLastName() == null || account.getLastName().isBlank()) {
+            this.validationHandler().append(new Error("'lastName' should not be null or blank"));
+        }
+
+        if (account.getEmail() == null || account.getEmail().isBlank()) {
+            this.validationHandler().append(new Error("'email' should not be null or blank"));
+        }
+
+        if (account.getPassword() == null || account.getPassword().isBlank()) {
+            this.validationHandler().append(new Error("'password' should not be null or blank"));
+        }
+
+        if (account.getPassword() != null && account.getPassword().length() < PASSWORD_MINIMUM_LENGTH) {
+            this.validationHandler().append(new Error("'password' should be at least " + PASSWORD_MINIMUM_LENGTH + " characters"));
+        }
     }
 }
