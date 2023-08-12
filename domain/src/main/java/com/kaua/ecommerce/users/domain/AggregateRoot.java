93f91e26014c245e5fb7ccd1bc4e3daca9241fdd
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 11:20:25 -0300

    feat(ddd): change aggregate root to abstract class

--- domain/src/main/java/com/kaua/ecommerce/users/domain/AggregateRoot.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/AggregateRoot.java
@@ -2,7 +2,7 @@ package com.kaua.ecommerce.users.domain;
 
 // Um aggregate root é uma entidade que é a raiz de um aggregate
 // Um aggregate é um conjunto de entidades e value objects que são tratados como uma unidade
-public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
 
     protected AggregateRoot(final ID id) {
         super(id);
