b6a730678a46b8204225bfda96301e0ecd2569ec
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 16:08:44 -0300

    feat(validation): add the NotificationHandler class)

---
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/validation/handler/NotificationHandler.java
@@ -0,0 +1,55 @@
+package com.kaua.ecommerce.users.domain.validation.handler;
+
+import com.kaua.ecommerce.users.domain.exceptions.DomainException;
+import com.kaua.ecommerce.users.domain.validation.Error;
+import com.kaua.ecommerce.users.domain.validation.Validation;
+import com.kaua.ecommerce.users.domain.validation.ValidationHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationHandler implements ValidationHandler {
+
+    private final List<Error> errors;
+
+    private NotificationHandler(final List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public static NotificationHandler create() {
+        return new NotificationHandler(new ArrayList<>());
+    }
+
+    public static NotificationHandler create(final Error aError) {
+        return new NotificationHandler(new ArrayList<>()).append(aError);
+    }
+
+    @Override
+    public NotificationHandler append(final Error aError) {
+        this.errors.add(aError);
+        return this;
+    }
+
+    @Override
+    public NotificationHandler append(ValidationHandler aHandler) {
+        this.errors.addAll(aHandler.getErrors());
+        return this;
+    }
+
+    @Override
+    public NotificationHandler validate(Validation aValidation) {
+        try {
+            aValidation.validate();
+        } catch (final DomainException ex) {
+            this.errors.addAll(ex.getErrors());
+        } catch (final Throwable t) {
+            this.errors.add(new Error(t.getMessage()));
+        }
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return this.errors;
+    }
+}
