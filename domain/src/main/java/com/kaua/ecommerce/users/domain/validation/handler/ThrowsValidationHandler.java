8fe1257c24cb17c54327b177f32ad0d4afc112db
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 15:15:47 -0300

    refactor(validation): remove List.of in ThrowsValidationHandler class

--- domain/src/main/java/com/kaua/ecommerce/users/domain/validation/handler/ThrowsValidationHandler.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/validation/handler/ThrowsValidationHandler.java
@@ -11,7 +11,7 @@ public class ThrowsValidationHandler implements ValidationHandler {
 
     @Override
     public ValidationHandler append(final Error aError) {
-        throw DomainException.with(List.of(aError));
+        throw DomainException.with(aError);
     }
 
     @Override
@@ -24,7 +24,7 @@ public class ThrowsValidationHandler implements ValidationHandler {
         try {
             aValidation.validate();
         } catch (final Exception ex) {
-            throw DomainException.with(List.of(new Error(ex.getMessage())));
+            throw DomainException.with(new Error(ex.getMessage()));
         }
         return this;
     }
