5ce1558aa15a9dedb95141996d2ef1feea239875
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-13 19:37:11 -0300

    feat(account): add the AccountGateway implementation

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/accounts/AccountMySQLGateway.java
@@ -0,0 +1,28 @@
+package com.kaua.ecommerce.users.infrastructure.accounts;
+
+import com.kaua.ecommerce.users.application.gateways.AccountGateway;
+import com.kaua.ecommerce.users.domain.accounts.Account;
+import com.kaua.ecommerce.users.infrastructure.accounts.persistence.AccountJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class AccountMySQLGateway implements AccountGateway {
+
+    private final AccountJpaRepository accountJpaRepository;
+
+    public AccountMySQLGateway(final AccountJpaRepository accountJpaRepository) {
+        this.accountJpaRepository = Objects.requireNonNull(accountJpaRepository);
+    }
+
+    @Override
+    public Account create(Account aAccount) {
+        return null;
+    }
+
+    @Override
+    public boolean existsByEmail(String aEmail) {
+        return false;
+    }
+}
