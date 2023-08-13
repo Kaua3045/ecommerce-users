379a20577b04f50248c3f30a5614f9de01d4dcbc
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-12 21:40:29 -0300

    feat(spring): set the default profile to development

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/Main.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/Main.java
@@ -2,10 +2,12 @@ package com.kaua.ecommerce.users.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(Main.class, args);
     }
 }
