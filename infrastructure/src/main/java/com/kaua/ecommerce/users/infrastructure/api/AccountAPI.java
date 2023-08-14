c31e351c4583d1ee96b24e4bac16a60ed0f9c64f
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 13:25:34 -0300

    feat(account): add the request body annotation and make implementation in account controller

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/AccountAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/AccountAPI.java
@@ -1,8 +1,10 @@
 package com.kaua.ecommerce.users.infrastructure.api;
 
+import com.kaua.ecommerce.users.infrastructure.accounts.models.CreateAccountApiInput;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "accounts")
@@ -12,5 +14,5 @@ public interface AccountAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    ResponseEntity<?> createAccount();
+    ResponseEntity<?> createAccount(@RequestBody CreateAccountApiInput input);
 }
--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/AccountController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/AccountController.java
@@ -1,13 +1,35 @@
 package com.kaua.ecommerce.users.infrastructure.api.controllers;
 
+import com.kaua.ecommerce.users.application.account.create.CreateAccountCommand;
+import com.kaua.ecommerce.users.application.account.create.CreateAccountUseCase;
+import com.kaua.ecommerce.users.infrastructure.accounts.models.CreateAccountApiInput;
 import com.kaua.ecommerce.users.infrastructure.api.AccountAPI;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController implements AccountAPI {
+
+    private final CreateAccountUseCase createAccountUseCase;
+
+    public AccountController(final CreateAccountUseCase createAccountUseCase) {
+        this.createAccountUseCase = createAccountUseCase;
+    }
+
     @Override
-    public ResponseEntity<?> createAccount() {
-        return null;
+    public ResponseEntity<?> createAccount(CreateAccountApiInput input) {
+        final var aCommand = CreateAccountCommand.with(
+                input.firstName(),
+                input.lastName(),
+                input.email(),
+                input.password()
+        );
+
+        final var aResult = this.createAccountUseCase.execute(aCommand);
+
+        return aResult.isLeft()
+                ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
+                : ResponseEntity.status(HttpStatus.CREATED).body(aResult.getRight());
     }
 }
