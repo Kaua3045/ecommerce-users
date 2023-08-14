11113fc1d8606eb32fca3a74e3106bfbf238d559
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 13:23:24 -0300

    refactor(account): change create account output return to string

--- application/src/main/java/com/kaua/ecommerce/users/application/account/create/CreateAccountOutput.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/account/create/CreateAccountOutput.java
@@ -3,9 +3,13 @@ package com.kaua.ecommerce.users.application.account.create;
 import com.kaua.ecommerce.users.domain.accounts.Account;
 import com.kaua.ecommerce.users.domain.accounts.AccountID;
 
-public record CreateAccountOutput(AccountID id) {
+public record CreateAccountOutput(String id) {
 
     public static CreateAccountOutput from(final Account aAccount) {
-        return new CreateAccountOutput(aAccount.getId());
+        return new CreateAccountOutput(aAccount.getId().getValue());
+    }
+
+    public static CreateAccountOutput from(final AccountID aId) {
+        return new CreateAccountOutput(aId.getValue());
     }
 }
