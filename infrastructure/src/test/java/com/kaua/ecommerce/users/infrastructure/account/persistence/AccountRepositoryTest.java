5506fd9d83c4295744892f7b75a542e637221669
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 11:10:26 -0300

    test(account): add the given an invalid null firstName when call save should return an exception

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/account/persistence/AccountRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.kaua.ecommerce.users.infrastructure.account.persistence;
+
+import com.kaua.ecommerce.users.domain.accounts.Account;
+import com.kaua.ecommerce.users.infrastructure.MySQLGatewayTest;
+import com.kaua.ecommerce.users.infrastructure.accounts.persistence.AccountJpaEntity;
+import com.kaua.ecommerce.users.infrastructure.accounts.persistence.AccountJpaRepository;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@MySQLGatewayTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountJpaRepository accountRepository;
+
+    @Test
+    public void givenAnInvalidNullFirstName_whenCallSave_shouldReturnAnException() {
+        final var expectedPropertyName = "firstName";
+        final var expectedErrorMessage = "not-null property references a null or transient value : com.kaua.ecommerce.users.infrastructure.accounts.persistence.AccountJpaEntity.firstName";
+
+        final var aAccount = Account.newAccount(
+                "Fulano",
+                "Silva",
+                "teste@teste.com",
+                "1234567Ab");
+
+        final var aEntity = AccountJpaEntity.toEntity(aAccount);
+        aEntity.setFirstName(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class,
+                () -> accountRepository.save(aEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class,
+                actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedErrorMessage, actualCause.getMessage());
+    }
+}
