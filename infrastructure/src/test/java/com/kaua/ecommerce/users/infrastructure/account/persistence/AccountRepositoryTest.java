1680b966065b1ba8a338db7d5404294e46e4e768
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 11:15:39 -0300

    test(account): add the given an invalid null updatedAt when call save should return an exception

--- infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/account/persistence/AccountRepositoryTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/account/persistence/AccountRepositoryTest.java
@@ -159,4 +159,28 @@ public class AccountRepositoryTest {
         Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
         Assertions.assertEquals(expectedErrorMessage, actualCause.getMessage());
     }
+
+    @Test
+    public void givenAnInvalidNullUpdatedAt_whenCallSave_shouldReturnAnException() {
+        final var expectedPropertyName = "updatedAt";
+        final var expectedErrorMessage = "not-null property references a null or transient value : com.kaua.ecommerce.users.infrastructure.accounts.persistence.AccountJpaEntity.updatedAt";
+
+        final var aAccount = Account.newAccount(
+                "Fulano",
+                "Silva",
+                "teste@teste.com",
+                "1234567Ab");
+
+        final var aEntity = AccountJpaEntity.toEntity(aAccount);
+        aEntity.setUpdatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class,
+                () -> accountRepository.save(aEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class,
+                actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedErrorMessage, actualCause.getMessage());
+    }
 }
