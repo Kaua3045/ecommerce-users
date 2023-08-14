728dba2cf42fde753ae280802693360a1cede17d
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 14:37:13 -0300

    feat(account): add the encrypter gateway injection

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AccountUseCaseConfig.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AccountUseCaseConfig.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.users.infrastructure.configurations;
 import com.kaua.ecommerce.users.application.account.create.CreateAccountUseCase;
 import com.kaua.ecommerce.users.application.account.create.DefaultCreateAccountUseCase;
 import com.kaua.ecommerce.users.application.gateways.AccountGateway;
+import com.kaua.ecommerce.users.application.gateways.EncrypterGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class AccountUseCaseConfig {
 
     private final AccountGateway accountGateway;
+    private final EncrypterGateway encrypterGateway;
 
-    public AccountUseCaseConfig(final AccountGateway accountGateway) {
+    public AccountUseCaseConfig(final AccountGateway accountGateway, final EncrypterGateway encrypterGateway) {
         this.accountGateway = accountGateway;
+        this.encrypterGateway = encrypterGateway;
     }
 
     @Bean
     public CreateAccountUseCase createAccountUseCase() {
-        return new DefaultCreateAccountUseCase(accountGateway);
+        return new DefaultCreateAccountUseCase(accountGateway, encrypterGateway);
     }
 }
