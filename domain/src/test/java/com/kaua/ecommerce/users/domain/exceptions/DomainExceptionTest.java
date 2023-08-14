6751cfa9549942b3af4f6e7e01a0c8b2a9bcc2ea
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-14 15:27:50 -0300

    test(exceptions): add the domain exception test

---
+++ domain/src/test/java/com/kaua/ecommerce/users/domain/exceptions/DomainExceptionTest.java
@@ -0,0 +1,20 @@
+package com.kaua.ecommerce.users.domain.exceptions;
+
+import com.kaua.ecommerce.users.domain.validation.Error;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class DomainExceptionTest {
+
+    @Test
+    public void givenAValidListOfError_whenCallDomainExceptionWith_ThenReturnDomainException() {
+        // given
+        var errors = List.of(new Error("Common Error"));
+        // when
+        var domainException = DomainException.with(errors);
+        // then
+        Assertions.assertEquals(errors, domainException.getErrors());
+    }
+}
