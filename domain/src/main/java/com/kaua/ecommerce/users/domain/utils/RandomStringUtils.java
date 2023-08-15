9e36dd9092e36cd0a79719bcfae0dc496afa6a07
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-15 12:36:21 -0300

    feat(utils): add the RandomStringUtils class

---
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/utils/RandomStringUtils.java
@@ -0,0 +1,22 @@
+package com.kaua.ecommerce.users.domain.utils;
+
+import java.util.Random;
+
+public final class RandomStringUtils {
+
+    private RandomStringUtils() {}
+
+    public static String generateValue(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            char randomChar = characters.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
