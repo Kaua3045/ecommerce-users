fa41e405ddd101e65c535d70561074b43fde2447
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-15 17:48:04 -0300

    test(either): add the EitherTest class

---
+++ application/src/test/java/com/kaua/ecommerce/users/application/either/EitherTest.java
@@ -0,0 +1,60 @@
+package com.kaua.ecommerce.users.application.either;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+public class EitherTest {
+
+    @Test
+    public void testLeft() {
+        Either<Integer, String> either = Either.left(42);
+        Assertions.assertTrue(either.isLeft());
+        Assertions.assertFalse(either.isRight());
+        Assertions.assertEquals(42, either.getLeft());
+        Assertions.assertThrows(NoSuchElementException.class, either::getRight);
+    }
+
+    @Test
+    public void testRight() {
+        Either<Integer, String> either = Either.right("Hello");
+        Assertions.assertFalse(either.isLeft());
+        Assertions.assertTrue(either.isRight());
+        Assertions.assertEquals("Hello", either.getRight());
+        Assertions.assertThrows(NoSuchElementException.class, either::getLeft);
+    }
+
+    @Test
+    public void testRightWithLeftMethods() {
+        Either<Integer, String> either = Either.right("Hello");
+        Assertions.assertFalse(either.isLeft());
+        Assertions.assertTrue(either.isRight());
+        Assertions.assertThrows(NoSuchElementException.class, either::getLeft);
+        Assertions.assertDoesNotThrow(() -> {
+            String value = either.getRight();
+            Assertions.assertEquals("Hello", value);
+        });
+    }
+
+    @Test
+    public void testLeftWithRightMethods() {
+        Either<Integer, String> either = Either.left(42);
+        Assertions.assertTrue(either.isLeft());
+        Assertions.assertFalse(either.isRight());
+        Assertions.assertThrows(NoSuchElementException.class, either::getRight);
+        Assertions.assertDoesNotThrow(() -> {
+            Integer value = either.getLeft();
+            Assertions.assertEquals(42, value);
+        });
+    }
+
+    @Test
+    public void testBaseMethods() {
+        Either<Integer, String> either = new Either.BaseMethods<>();
+        Assertions.assertThrows(UnsupportedOperationException.class, either::isLeft);
+        Assertions.assertThrows(UnsupportedOperationException.class, either::isRight);
+        Assertions.assertThrows(UnsupportedOperationException.class, either::getLeft);
+        Assertions.assertThrows(UnsupportedOperationException.class, either::getRight);
+    }
+}
