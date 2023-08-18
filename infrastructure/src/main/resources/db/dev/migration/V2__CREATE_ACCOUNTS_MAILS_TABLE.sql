48b8fb07f18c21dd701988ec56e836353757155a
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-18 16:30:30 -0300

    feat(migrations): add the create accounts mails table

---
+++ infrastructure/src/main/resources/db/dev/migration/V2__CREATE_ACCOUNTS_MAILS_TABLE.sql
@@ -0,0 +1,10 @@
+CREATE TABLE accounts_mails (
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    token VARCHAR(36) UNIQUE NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    expires_at DATETIME(6) NOT NULL,
+    account_id VARCHAR(36) NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT FK_ACCOUNTS_MAILS_ACCOUNT_ID FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
