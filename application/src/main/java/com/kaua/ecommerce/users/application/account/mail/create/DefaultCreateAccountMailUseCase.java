2fe135a6ccae1a50be52ea14d081acceeb8a1255
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-19 15:40:11 -0300

    feat(account-mail): add the queue gateway to send message in mail queue

--- application/src/main/java/com/kaua/ecommerce/users/application/account/mail/create/DefaultCreateAccountMailUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/account/mail/create/DefaultCreateAccountMailUseCase.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.users.application.account.mail.create;
 import com.kaua.ecommerce.users.application.either.Either;
 import com.kaua.ecommerce.users.application.gateways.AccountGateway;
 import com.kaua.ecommerce.users.application.gateways.AccountMailGateway;
+import com.kaua.ecommerce.users.application.gateways.QueueGateway;
 import com.kaua.ecommerce.users.domain.accounts.Account;
 import com.kaua.ecommerce.users.domain.accounts.mail.AccountMail;
 import com.kaua.ecommerce.users.domain.exceptions.NotFoundException;
@@ -15,10 +16,16 @@ public class DefaultCreateAccountMailUseCase extends CreateAccountMailUseCase {
 
     private final AccountMailGateway accountMailGateway;
     private final AccountGateway accountGateway;
+    private final QueueGateway queueGateway;
 
-    public DefaultCreateAccountMailUseCase(final AccountMailGateway accountMailGateway, final AccountGateway accountGateway) {
+    public DefaultCreateAccountMailUseCase(
+            final AccountMailGateway accountMailGateway,
+            final AccountGateway accountGateway,
+            final QueueGateway queueGateway
+    ) {
         this.accountMailGateway = Objects.requireNonNull(accountMailGateway);
         this.accountGateway = Objects.requireNonNull(accountGateway);
+        this.queueGateway = Objects.requireNonNull(queueGateway);
     }
 
     @Override
@@ -51,6 +58,11 @@ public class DefaultCreateAccountMailUseCase extends CreateAccountMailUseCase {
 
         return notification.hasError()
                 ? Either.left(notification)
-                : Either.right(this.accountMailGateway.create(aAccountMail));
+                : Either.right(this.createAccountMail(aAccountMail));
+    }
+
+    private AccountMail createAccountMail(AccountMail aAccountMail) {
+        this.queueGateway.send(aAccountMail);
+        return this.accountMailGateway.create(aAccountMail);
     }
 }
