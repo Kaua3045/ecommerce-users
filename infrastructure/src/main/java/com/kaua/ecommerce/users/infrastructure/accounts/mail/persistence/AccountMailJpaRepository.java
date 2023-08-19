c3439a0c829ca003f27d77e0bc95f044da365429
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-19 19:35:18 -0300

    feat(account): add the findByToken method in account mail jpa repository interface

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/accounts/mail/persistence/AccountMailJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/accounts/mail/persistence/AccountMailJpaRepository.java
@@ -3,8 +3,11 @@ package com.kaua.ecommerce.users.infrastructure.accounts.mail.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountMailJpaRepository extends JpaRepository<AccountMailJpaEntity, String> {
 
     List<AccountMailJpaEntity> findAllByAccountId(String accountId);
+
+    Optional<AccountMailJpaEntity> findByToken(String token);
 }
