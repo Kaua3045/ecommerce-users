18415ba0468cba1ce0455b1ce5499ab24b53ef87
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-19 15:41:51 -0300

    feat(config): add the queue config class

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/QueueConfig.java
@@ -0,0 +1,20 @@
+package com.kaua.ecommerce.users.infrastructure.configurations;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kaua.ecommerce.users.application.gateways.QueueGateway;
+import com.kaua.ecommerce.users.infrastructure.services.local.InMemoryQueueService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class QueueConfig {
+
+    @Bean
+    @Qualifier("mail")
+    @Profile({"development", "test"})
+    public QueueGateway localQueueGateway(ObjectMapper mapper) {
+        return new InMemoryQueueService(mapper);
+    }
+}
