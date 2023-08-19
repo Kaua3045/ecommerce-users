a6e45840bfb89a735cea615f905797ed3bd13e7f
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-19 19:22:28 -0300

    feat(account-mail): add the findByToken method in account mail gateway

--- application/src/main/java/com/kaua/ecommerce/users/application/gateways/AccountMailGateway.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/gateways/AccountMailGateway.java
@@ -3,12 +3,15 @@ package com.kaua.ecommerce.users.application.gateways;
 import com.kaua.ecommerce.users.domain.accounts.mail.AccountMail;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountMailGateway {
 
     AccountMail create(AccountMail accountMail);
 
     List<AccountMail> findAllByAccountId(String accountId);
+
+    Optional<AccountMail> findByToken(String token);
 
     void deleteById(String id);
 }
