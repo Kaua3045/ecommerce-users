ee0e6097d884a8b2454b080589341879accc7288
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-20 12:58:08 -0300

    feat(account): remove confirmation mail implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/AccountController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/AccountController.java
@@ -4,18 +4,12 @@ import com.kaua.ecommerce.users.application.account.create.CreateAccountCommand;
 import com.kaua.ecommerce.users.application.account.create.CreateAccountUseCase;
 import com.kaua.ecommerce.users.application.account.mail.confirm.ConfirmAccountMailCommand;
 import com.kaua.ecommerce.users.application.account.mail.confirm.ConfirmAccountMailUseCase;
-import com.kaua.ecommerce.users.application.account.mail.create.CreateAccountMailCommand;
 import com.kaua.ecommerce.users.application.account.mail.create.CreateAccountMailUseCase;
-import com.kaua.ecommerce.users.domain.accounts.mail.AccountMailType;
-import com.kaua.ecommerce.users.domain.utils.IdUtils;
-import com.kaua.ecommerce.users.domain.utils.InstantUtils;
 import com.kaua.ecommerce.users.infrastructure.accounts.models.CreateAccountApiInput;
 import com.kaua.ecommerce.users.infrastructure.api.AccountAPI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.temporal.ChronoUnit;
 
 @RestController
 public class AccountController implements AccountAPI {
@@ -44,19 +38,6 @@ public class AccountController implements AccountAPI {
         );
 
         final var aResult = this.createAccountUseCase.execute(aCommand);
-
-        if (aResult.isRight()) {
-            final var aCommandMail = CreateAccountMailCommand.with(
-                    aResult.getRight().id(),
-                    IdUtils.generate().replace("-", ""),
-                    AccountMailType.ACCOUNT_CONFIRMATION,
-                    "Account Confirmation",
-                    InstantUtils.now().plus(1, ChronoUnit.HOURS)
-            );
-
-            final var aAccountMailResut = this.createAccountMailUseCase.execute(aCommandMail);
-            // TODO: add debug log in aAccountMailResut.isLeft()
-        }
 
         return aResult.isLeft()
                 ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
