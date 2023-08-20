d162079cffdae214b4c27ecff9c1742aecf887d4
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-20 11:26:58 -0300

    feat(event): add the domain events list in entity and aggregate root

--- domain/src/main/java/com/kaua/ecommerce/users/domain/Entity.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/Entity.java
@@ -1,7 +1,12 @@
 package com.kaua.ecommerce.users.domain;
 
+import com.kaua.ecommerce.users.domain.event.DomainEvent;
+import com.kaua.ecommerce.users.domain.event.DomainEventPublisher;
 import com.kaua.ecommerce.users.domain.validation.ValidationHandler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 // Entity definida por um ID
@@ -10,17 +15,39 @@ import java.util.Objects;
 // Em uma entity o id é constante, já os atributos podem ser alterados
 public abstract class Entity<ID extends Identifier> {
 
-    protected final ID id;
+    private final ID id;
+    private final List<DomainEvent> domainEvents;
 
-    protected Entity(final ID id) {
+    protected Entity(final ID id, final List<DomainEvent> domainEvents) {
         Objects.requireNonNull(id, "'id' should not be null");
         this.id = id;
+        this.domainEvents = new ArrayList<>(domainEvents == null ? Collections.emptyList() : domainEvents);
     }
 
     public abstract void validate(ValidationHandler handler);
 
     public ID getId() {
         return id;
+    }
+
+    public List<DomainEvent> getDomainEvents() {
+        return Collections.unmodifiableList(domainEvents);
+    }
+
+    public void publishDomainEvent(final DomainEventPublisher publisher) {
+        if (publisher == null) {
+            return;
+        }
+
+        getDomainEvents().forEach(publisher::publish);
+
+        this.domainEvents.clear();
+    }
+
+    public void registerEvent(final DomainEvent event) {
+        if (event != null) {
+            this.domainEvents.add(event);
+        }
     }
 
     @Override
