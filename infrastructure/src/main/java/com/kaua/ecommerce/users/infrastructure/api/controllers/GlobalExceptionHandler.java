f29d747ef646eeb31e4a582d2643c5a7124411a8
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-20 16:19:18 -0300

    feat(exception): add the NotFoundException global handler

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.kaua.ecommerce.users.infrastructure.api.controllers;
 
 import com.kaua.ecommerce.users.domain.exceptions.DomainException;
+import com.kaua.ecommerce.users.domain.exceptions.NotFoundException;
 import com.kaua.ecommerce.users.infrastructure.utils.ApiError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +14,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<?> handleDomainException(final DomainException exception) {
         return ResponseEntity.unprocessableEntity().body(ApiError.from(exception));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleDomainException(final NotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(exception));
     }
 }
