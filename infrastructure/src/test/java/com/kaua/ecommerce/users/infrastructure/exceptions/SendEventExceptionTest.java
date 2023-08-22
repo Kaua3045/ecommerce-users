5fa88dabf85e64f53a99ec83376fec321a6472fb
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-22 12:08:50 -0300

    test(exception): add the SendEventExceptionTest

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/exceptions/SendEventExceptionTest.java
@@ -0,0 +1,19 @@
+package com.kaua.ecommerce.users.infrastructure.exceptions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SendEventExceptionTest {
+
+    @Test
+    public void givenAValidErrorMessage_whenSendEventExceptionIsCalled_thenASendEventExceptionShouldBeThrown() {
+        // Arrange
+        final var aErrorMessage = "Error sending event to SNS";
+
+        // Act
+        final var aSendEventException = new SendEventException(aErrorMessage);
+
+        // Assert
+        Assertions.assertEquals(aErrorMessage, aSendEventException.getMessage());
+    }
+}
