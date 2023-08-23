565aa1fb7600cbea9318bf9589317456ba8aba04
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-23 11:29:13 -0300

    feat(rabbitmq): add the rabbitmq admin configure

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AmqpConfig.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AmqpConfig.java
@@ -2,7 +2,9 @@ package com.kaua.ecommerce.users.infrastructure.configurations;
 
 import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountCreatedEvent;
 import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountCreatedGenerateMailCodeEvent;
+import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountEvents;
 import com.kaua.ecommerce.users.infrastructure.configurations.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +20,51 @@ public class AmqpConfig {
     }
 
     @Bean
-    @ConfigurationProperties("amqp.queues.account-generate-confirmation-code-mail")
+    @ConfigurationProperties("amqp.queues.account-created-generate-confirmation-code-mail")
     @AccountCreatedGenerateMailCodeEvent
-    public QueueProperties accountGenerateConfirmationMailQueueProperties() {
+    public QueueProperties accountCreatedGenerateConfirmationMailQueueProperties() {
         return new QueueProperties();
+    }
+
+    @Configuration
+    static class Admin {
+
+        @Bean
+        @AccountEvents
+        public Exchange accountEventsExchange(@AccountCreatedEvent final QueueProperties props) {
+            return new FanoutExchange(props.getExchange());
+        }
+
+        @Bean
+        @AccountCreatedEvent
+        public Queue accountCreatedQueue(@AccountCreatedEvent final QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @AccountCreatedGenerateMailCodeEvent
+        public Queue accountCreatedGenerateMailCodeEventQueue(
+                @AccountCreatedGenerateMailCodeEvent final QueueProperties props
+        ) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @AccountCreatedEvent
+        public Binding accountCreatedBinding(
+                @AccountEvents FanoutExchange exchange,
+                @AccountCreatedEvent Queue queue
+        ) {
+            return BindingBuilder.bind(queue).to(exchange);
+        }
+
+        @Bean
+        @AccountCreatedGenerateMailCodeEvent
+        public Binding accountCreatedGenerateMailCodeEventBinding(
+                @AccountEvents FanoutExchange exchange,
+                @AccountCreatedGenerateMailCodeEvent Queue queue
+        ) {
+            return BindingBuilder.bind(queue).to(exchange);
+        }
     }
 }
