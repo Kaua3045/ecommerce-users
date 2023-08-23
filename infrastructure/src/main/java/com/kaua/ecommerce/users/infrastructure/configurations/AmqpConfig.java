e7753fd93f563c7216cfbf7ace6519112280c8a0
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-23 11:16:45 -0300

    feat(rabbitmq): add the generate confirmatio mail code annotation

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AmqpConfig.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/AmqpConfig.java
@@ -1,5 +1,7 @@
 package com.kaua.ecommerce.users.infrastructure.configurations;
 
+import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountCreatedEvent;
+import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountCreatedGenerateMailCodeEvent;
 import com.kaua.ecommerce.users.infrastructure.configurations.properties.amqp.QueueProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +12,14 @@ public class AmqpConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.account-created")
+    @AccountCreatedEvent
     public QueueProperties accountCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.account-generate-confirmation-code-mail")
+    @AccountCreatedGenerateMailCodeEvent
     public QueueProperties accountGenerateConfirmationMailQueueProperties() {
         return new QueueProperties();
     }
