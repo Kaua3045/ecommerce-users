c93f0b2ebd7bc3f1befb6b086719d5c1911e5c31
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-23 11:33:46 -0300

    feat(rabbitmq): add the rabbitmq event service and configure event bean

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/EventConfig.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/configurations/EventConfig.java
@@ -1,8 +1,12 @@
 package com.kaua.ecommerce.users.infrastructure.configurations;
 
 import com.kaua.ecommerce.users.infrastructure.configurations.annotations.AccountCreatedEvent;
+import com.kaua.ecommerce.users.infrastructure.configurations.properties.amqp.QueueProperties;
 import com.kaua.ecommerce.users.infrastructure.services.EventService;
+import com.kaua.ecommerce.users.infrastructure.services.impl.RabbitEventService;
 import com.kaua.ecommerce.users.infrastructure.services.local.InMemoryEventService;
+import org.springframework.amqp.rabbit.core.RabbitOperations;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,8 +16,18 @@ public class EventConfig {
 
     @Bean
     @AccountCreatedEvent
-    @Profile({"development", "test"})
+    @Profile({"development"})
     public EventService localEventService() {
         return new InMemoryEventService();
+    }
+
+    @Bean
+    @AccountCreatedEvent
+    @ConditionalOnMissingBean
+    public EventService rabbitEventService(
+            @AccountCreatedEvent QueueProperties props,
+            final RabbitOperations ops
+    ) {
+        return new RabbitEventService(props.getExchange(), props.getRoutingKey(), ops);
     }
 }
