e86d2abd41846a565098c634bbcc853f2d3ab587
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-30 11:43:05 -0300

    test(storage-service): add the in memory storage service tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/service/local/InMemoryStorageServiceTest.java
@@ -0,0 +1,88 @@
+package com.kaua.ecommerce.users.infrastructure.service.local;
+
+import com.kaua.ecommerce.users.domain.utils.IdUtils;
+import com.kaua.ecommerce.users.domain.utils.Resource;
+import com.kaua.ecommerce.users.infrastructure.services.local.InMemoryStorageService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+public class InMemoryStorageServiceTest {
+
+    private InMemoryStorageService target = new InMemoryStorageService();
+
+    @BeforeEach
+    void cleanUp() {
+        this.target.clear();
+    }
+
+    @Test
+    void givenValidResource_whenCallUploadFile_thenShouldUploadFile() {
+        // given
+        final var aFileName = "avatar";
+        final var aResource = Resource.with(
+                InputStream.nullInputStream(),
+                "image/png",
+                aFileName
+        );
+        final var aKey = IdUtils.generate();
+        final var expectedUrl = "http://localhost:8080/files/" + aKey + "-" + aFileName + ".png";
+
+        // when
+        this.target.uploadFile(aKey, aResource);
+
+        // then
+        Assertions.assertEquals(expectedUrl, this.target.getFileUrl(aKey + "-avatar").get());
+    }
+
+    @Test
+    void givenValidKey_whenCallDeleteFile_thenShouldDeleteFile() {
+        // given
+        final var aFileName = "avatar";
+        final var aResource = Resource.with(
+                InputStream.nullInputStream(),
+                "image/png",
+                aFileName
+        );
+        final var aKey = IdUtils.generate();
+        this.target.uploadFile(aKey, aResource);
+
+        // when
+        this.target.deleteFile(aKey + "-avatar");
+
+        // then
+        Assertions.assertTrue(this.target.getFileUrl(aKey + "-avatar").isEmpty());
+    }
+
+    @Test
+    void givenValidKey_whenCallGetFileUrl_thenShouldReturnFileUrl() {
+        // given
+        final var aFileName = "avatar";
+        final var aResource = Resource.with(
+                InputStream.nullInputStream(),
+                "image/png",
+                aFileName
+        );
+        final var aKey = IdUtils.generate();
+        final var expectedUrl = "http://localhost:8080/files/" + aKey + "-" + aFileName + ".png";
+        this.target.uploadFile(aKey, aResource);
+
+        // when
+        final var aUrl = this.target.getFileUrl(aKey + "-avatar").get();
+        // then
+        Assertions.assertEquals(expectedUrl, aUrl);
+    }
+
+    @Test
+    void givenAnInvalid_whenCallGetFileUrl_thenShouldReturnEmpty() {
+        // given
+        final var aKey = IdUtils.generate();
+
+        // when
+        final var aUrl = this.target.getFileUrl(aKey + "-avatar");
+        // then
+        Assertions.assertTrue(aUrl.isEmpty());
+    }
+}
