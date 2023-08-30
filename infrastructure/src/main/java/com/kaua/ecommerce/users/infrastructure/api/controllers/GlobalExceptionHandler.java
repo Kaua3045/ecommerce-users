162eb331e8ab4c49cfe81d34580880a171d6af10
Kauã Pereira <zkauacodes@hotmail.com>
2023-08-30 14:27:35 -0300

    feat(exception): add the log to show errors

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.kaua.ecommerce.users.infrastructure.api.controllers;
 import com.kaua.ecommerce.users.domain.exceptions.DomainException;
 import com.kaua.ecommerce.users.domain.exceptions.NotFoundException;
 import com.kaua.ecommerce.users.infrastructure.utils.ApiError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ApiError> handleDomainException(final DomainException exception) {
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGenericException(final Exception exception) {
+        log.debug("Internal server error", exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiError.from("Internal server error"));
     }
 }
