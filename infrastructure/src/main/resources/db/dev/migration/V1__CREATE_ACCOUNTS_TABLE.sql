db449609c089d27f35734282e361550264c85b55
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-02 09:49:38 -0300

    refactor(account): add account_id column annotation

--- infrastructure/src/main/resources/db/dev/migration/V1__CREATE_ACCOUNTS_TABLE.sql
+++ infrastructure/src/main/resources/db/dev/migration/V1__CREATE_ACCOUNTS_TABLE.sql
@@ -1,5 +1,5 @@
 CREATE TABLE accounts (
-    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    account_id VARCHAR(36) PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
