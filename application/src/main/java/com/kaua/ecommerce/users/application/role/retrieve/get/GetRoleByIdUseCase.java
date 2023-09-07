9eccdbd2c832fc315280c78bc507dfe137b9feb8
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-07 18:35:09 -0300

    feat(get-role-by-id-usecase): add the get role by id usecase abstraction

---
+++ application/src/main/java/com/kaua/ecommerce/users/application/role/retrieve/get/GetRoleByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.users.application.role.retrieve.get;
+
+import com.kaua.ecommerce.users.application.UseCase;
+
+public abstract class GetRoleByIdUseCase extends UseCase<GetRoleByIdCommand, GetRoleByIdOutput> {
+}
