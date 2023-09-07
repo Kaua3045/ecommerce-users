a28bffc7ae428a6f3605377f700f13b085d1606d
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-07 11:22:48 -0300

    feat(role-gateway): add the findById and update methods

--- application/src/main/java/com/kaua/ecommerce/users/application/gateways/RoleGateway.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/gateways/RoleGateway.java
@@ -2,9 +2,15 @@ package com.kaua.ecommerce.users.application.gateways;
 
 import com.kaua.ecommerce.users.domain.roles.Role;
 
+import java.util.Optional;
+
 public interface RoleGateway {
 
     Role create(Role aRole);
 
     boolean existsByName(String aName);
+
+    Optional<Role> findById(String aId);
+
+    Role update(Role aRole);
 }
