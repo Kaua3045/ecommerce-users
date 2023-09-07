cd1a7d762aaa0ff1a15b35aeba0656b41e723665
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-07 11:41:32 -0300

    feat(role-api): add the update role route implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/RoleController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/controllers/RoleController.java
@@ -2,8 +2,11 @@ package com.kaua.ecommerce.users.infrastructure.api.controllers;
 
 import com.kaua.ecommerce.users.application.role.create.CreateRoleCommand;
 import com.kaua.ecommerce.users.application.role.create.CreateRoleUseCase;
+import com.kaua.ecommerce.users.application.role.update.UpdateRoleCommand;
+import com.kaua.ecommerce.users.application.role.update.UpdateRoleUseCase;
 import com.kaua.ecommerce.users.infrastructure.api.RoleAPI;
 import com.kaua.ecommerce.users.infrastructure.roles.models.CreateRoleApiInput;
+import com.kaua.ecommerce.users.infrastructure.roles.models.UpdateRoleApiInput;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController implements RoleAPI {
 
     private final CreateRoleUseCase createRoleUseCase;
+    private final UpdateRoleUseCase updateRoleUseCase;
 
-    public RoleController(final CreateRoleUseCase createRoleUseCase) {
+    public RoleController(final CreateRoleUseCase createRoleUseCase, final UpdateRoleUseCase updateRoleUseCase) {
         this.createRoleUseCase = createRoleUseCase;
+        this.updateRoleUseCase = updateRoleUseCase;
     }
 
     @Override
@@ -30,5 +35,21 @@ public class RoleController implements RoleAPI {
         return aResult.isLeft()
                 ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
                 : ResponseEntity.status(HttpStatus.CREATED).body(aResult.getRight());
+    }
+
+    @Override
+    public ResponseEntity<?> updateRole(String id, UpdateRoleApiInput input) {
+        final var aCommand = UpdateRoleCommand.with(
+                id,
+                input.name(),
+                input.description(),
+                input.roleType()
+        );
+
+        final var aResult = this.updateRoleUseCase.execute(aCommand);
+
+        return aResult.isLeft()
+                ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
+                : ResponseEntity.ok().body(aResult.getRight());
     }
 }
