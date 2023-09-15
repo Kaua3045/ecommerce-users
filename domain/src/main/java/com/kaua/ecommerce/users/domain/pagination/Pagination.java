52911061f9dc6095d5efc2416f05cb00a302ad08
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-15 11:46:36 -0300

    feat(pagination): add the map method

--- domain/src/main/java/com/kaua/ecommerce/users/domain/pagination/Pagination.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/pagination/Pagination.java
@@ -1,6 +1,7 @@
 package com.kaua.ecommerce.users.domain.pagination;
 
 import java.util.List;
+import java.util.function.Function;
 
 public record Pagination<T>(
         int currentPage,
@@ -9,4 +10,18 @@ public record Pagination<T>(
         long total,
         List<T> items
 ) {
+
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+        final List<R> aNewList = this.items.stream()
+                .map(mapper)
+                .toList();
+
+        return new Pagination<>(
+                currentPage(),
+                perPage(),
+                totalPage(),
+                total(),
+                aNewList
+        );
+    }
 }
