c6639993fe8bcf46731f21a75f8684482511089f
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-15 11:44:23 -0300

    feat(list-role-output): add the from static method

--- application/src/main/java/com/kaua/ecommerce/users/application/role/retrieve/list/ListRoleOutput.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/role/retrieve/list/ListRoleOutput.java
@@ -1,6 +1,8 @@
 package com.kaua.ecommerce.users.application.role.retrieve.list;
 
+import com.kaua.ecommerce.users.domain.roles.Role;
 import com.kaua.ecommerce.users.domain.roles.RoleID;
+import com.kaua.ecommerce.users.domain.roles.RoleTypes;
 
 import java.time.Instant;
 
@@ -8,8 +10,16 @@ public record ListRoleOutput(
         RoleID id,
         String name,
         String description,
-        String type,
-        Instant createdAt,
-        Instant updatedAt
+        RoleTypes type,
+        Instant createdAt
 ) {
+
+    public static ListRoleOutput from(final Role aRole) {
+        return new ListRoleOutput(
+                aRole.getId(),
+                aRole.getName(),
+                aRole.getDescription(),
+                aRole.getRoleType(),
+                aRole.getCreatedAt());
+    }
 }
