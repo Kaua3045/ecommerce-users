75b03e28e69703a23cfe8b86437d367f6915dd38
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-15 13:12:21 -0300

    feat(role-api): add the list roles route

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/RoleAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/RoleAPI.java
@@ -1,7 +1,9 @@
 package com.kaua.ecommerce.users.infrastructure.api;
 
+import com.kaua.ecommerce.users.domain.pagination.Pagination;
 import com.kaua.ecommerce.users.infrastructure.roles.models.CreateRoleApiInput;
-import com.kaua.ecommerce.users.infrastructure.roles.models.GetRoleOutput;
+import com.kaua.ecommerce.users.infrastructure.roles.models.GetRoleResponse;
+import com.kaua.ecommerce.users.infrastructure.roles.models.ListRoleResponse;
 import com.kaua.ecommerce.users.infrastructure.roles.models.UpdateRoleApiInput;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +28,16 @@ public interface RoleAPI {
             value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    GetRoleOutput getRole(@PathVariable String id);
+    GetRoleResponse getRole(@PathVariable String id);
+
+    @GetMapping
+    Pagination<ListRoleResponse> listRoles(
+            @RequestParam(name = "search", required = false, defaultValue = "") final String search,
+            @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
+            @RequestParam(name = "perPage", required = false, defaultValue = "10") final int perPage,
+            @RequestParam(name = "sort", required = false, defaultValue = "name") final String sort,
+            @RequestParam(name = "dir", required = false, defaultValue = "asc") final String direction
+    );
 
     @DeleteMapping("{id}")
     ResponseEntity<?> deleteRole(@PathVariable String id);
