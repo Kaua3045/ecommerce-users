5167995fb9c85b651ed2912b488b040d0436d10b
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-15 12:44:06 -0300

    feat(role-jpa-repository): add the findAll method

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/roles/persistence/RoleJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/roles/persistence/RoleJpaRepository.java
@@ -1,8 +1,13 @@
 package com.kaua.ecommerce.users.infrastructure.roles.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleJpaRepository extends JpaRepository<RoleJpaEntity, String> {
 
     boolean existsByName(String name);
+
+    Page<RoleJpaEntity> findAll(Specification<RoleJpaEntity> whereClause, Pageable page);
 }
