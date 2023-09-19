98bd1b4c3fb4f4889f38aa921b98554b18274a73
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-18 21:15:52 -0300

    fix(update-account-role-usecase): change return to use either

--- application/src/main/java/com/kaua/ecommerce/users/application/account/update/role/UpdateAccountRoleUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/account/update/role/UpdateAccountRoleUseCase.java
@@ -1,6 +1,9 @@
 package com.kaua.ecommerce.users.application.account.update.role;
 
 import com.kaua.ecommerce.users.application.UseCase;
+import com.kaua.ecommerce.users.application.either.Either;
+import com.kaua.ecommerce.users.domain.validation.handler.NotificationHandler;
 
-public abstract class UpdateAccountRoleUseCase extends UseCase<UpdateAccountRoleCommand, UpdateAccountRoleOutput> {
+public abstract class UpdateAccountRoleUseCase extends
+        UseCase<UpdateAccountRoleCommand, Either<NotificationHandler, UpdateAccountRoleOutput>> {
 }
