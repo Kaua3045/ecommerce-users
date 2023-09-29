b9a12df491c1907ceb136ac4f478ab5bbef455d1
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-29 16:23:34 -0300

    test(permission-gateway): add the permission mysql gateway tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/infrastructure/permission/PermissionMySQLGatewayTest.java
@@ -0,0 +1,99 @@
+package com.kaua.ecommerce.users.infrastructure.permission;
+
+import com.kaua.ecommerce.users.IntegrationTest;
+import com.kaua.ecommerce.users.domain.permissions.Permission;
+import com.kaua.ecommerce.users.infrastructure.permissions.PermissionMySQLGateway;
+import com.kaua.ecommerce.users.infrastructure.permissions.persistence.PermissionJpaEntity;
+import com.kaua.ecommerce.users.infrastructure.permissions.persistence.PermissionJpaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class PermissionMySQLGatewayTest {
+
+    @Autowired
+    private PermissionMySQLGateway permissionGateway;
+
+    @Autowired
+    private PermissionJpaRepository permissionRepository;
+
+    @Test
+    void givenAValidPermissionWithDescription_whenCallCreate_shouldReturnANewPermission() {
+        final var aName = "create-role";
+        final var aDescription = "Create a new role";
+
+        final var aPermission = Permission.newPermission(aName, aDescription);
+
+        Assertions.assertEquals(0, permissionRepository.count());
+
+        final var actualPermission = permissionGateway.create(aPermission);
+
+        Assertions.assertEquals(1, permissionRepository.count());
+
+        Assertions.assertEquals(aPermission.getId(), actualPermission.getId());
+        Assertions.assertEquals(aPermission.getName(), actualPermission.getName());
+        Assertions.assertEquals(aPermission.getDescription(), actualPermission.getDescription());
+        Assertions.assertEquals(aPermission.getCreatedAt(), actualPermission.getCreatedAt());
+        Assertions.assertEquals(aPermission.getUpdatedAt(), actualPermission.getUpdatedAt());
+
+        final var actualEntity = permissionRepository.findById(actualPermission.getId().getValue()).get();
+
+        Assertions.assertEquals(aPermission.getId().getValue(), actualEntity.getId());
+        Assertions.assertEquals(aName, actualEntity.getName());
+        Assertions.assertEquals(aDescription, actualEntity.getDescription());
+        Assertions.assertEquals(aPermission.getCreatedAt(), actualEntity.getCreatedAt());
+        Assertions.assertEquals(aPermission.getUpdatedAt(), actualEntity.getUpdatedAt());
+    }
+
+    @Test
+    void givenAValidPermissionWithNullDescription_whenCallCreate_shouldReturnANewPermission() {
+        final var aName = "create-role";
+        final String aDescription = null;
+
+        final var aPermission = Permission.newPermission(aName, aDescription);
+
+        Assertions.assertEquals(0, permissionRepository.count());
+
+        final var actualPermission = permissionGateway.create(aPermission);
+
+        Assertions.assertEquals(1, permissionRepository.count());
+
+        Assertions.assertEquals(aPermission.getId(), actualPermission.getId());
+        Assertions.assertEquals(aPermission.getName(), actualPermission.getName());
+        Assertions.assertEquals(aPermission.getDescription(), actualPermission.getDescription());
+        Assertions.assertEquals(aPermission.getCreatedAt(), actualPermission.getCreatedAt());
+        Assertions.assertEquals(aPermission.getUpdatedAt(), actualPermission.getUpdatedAt());
+
+        final var actualEntity = permissionRepository.findById(actualPermission.getId().getValue()).get();
+
+        Assertions.assertEquals(aPermission.getId().getValue(), actualEntity.getId());
+        Assertions.assertEquals(aName, actualEntity.getName());
+        Assertions.assertEquals(aDescription, actualEntity.getDescription());
+        Assertions.assertEquals(aPermission.getCreatedAt(), actualEntity.getCreatedAt());
+        Assertions.assertEquals(aPermission.getUpdatedAt(), actualEntity.getUpdatedAt());
+    }
+
+
+    @Test
+    void givenAValidNameButNotExistis_whenCallExistsByName_shouldReturnFalse() {
+        final var aName = "create-role";
+
+        Assertions.assertEquals(0, permissionRepository.count());
+        Assertions.assertFalse(permissionGateway.existsByName(aName));
+    }
+
+    @Test
+    void givenAValidExistingName_whenCallExistsByName_shouldReturnTrue() {
+        final var aName = "create-role";
+        final var aDescription = "Create a new role";
+
+        final var aPermission = Permission.newPermission(aName, aDescription);
+
+        Assertions.assertEquals(0, permissionRepository.count());
+        permissionRepository.save(PermissionJpaEntity.toEntity(aPermission));
+        Assertions.assertEquals(1, permissionRepository.count());
+
+        Assertions.assertTrue(permissionGateway.existsByName(aName));
+    }
+}
