77396c5bbd110dcdbbbba1af2a10147027c18694
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-28 21:46:17 -0300

    feat(create-permission-output): add the create permission output record class

--- application/src/main/java/com/kaua/ecommerce/users/application/permission/create/CreatePermissionCommand.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/permission/create/CreatePermissionCommand.java
@@ -2,7 +2,7 @@ package com.kaua.ecommerce.users.application.permission.create;
 
 public record CreatePermissionCommand(String name, String description) {
 
-    public static CreatePermissionCommand with(String name, String description) {
+    public static CreatePermissionCommand with(final String name, final String description) {
         return new CreatePermissionCommand(name, description);
     }
 }
---
+++ application/src/main/java/com/kaua/ecommerce/users/application/permission/create/CreatePermissionOutput.java
@@ -0,0 +1,14 @@
+package com.kaua.ecommerce.users.application.permission.create;
+
+import com.kaua.ecommerce.users.domain.permissions.Permission;
+
+public record CreatePermissionOutput(String id) {
+
+    public static CreatePermissionOutput from(final String id) {
+        return new CreatePermissionOutput(id);
+    }
+
+    public static CreatePermissionOutput from(final Permission aPermission) {
+        return new CreatePermissionOutput(aPermission.getId().getValue());
+    }
+}
