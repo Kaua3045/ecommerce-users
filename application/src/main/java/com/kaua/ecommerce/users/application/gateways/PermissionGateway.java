2e9f93b11692805460180166da97078830cc3f6d
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-28 21:43:42 -0300

    feat(permission-gateway): add the permission gateway interface, add create and existsByName methods in interface

---
+++ application/src/main/java/com/kaua/ecommerce/users/application/gateways/PermissionGateway.java
@@ -0,0 +1,10 @@
+package com.kaua.ecommerce.users.application.gateways;
+
+import com.kaua.ecommerce.users.domain.permissions.Permission;
+
+public interface PermissionGateway {
+
+    Permission create(Permission aPermission);
+
+    boolean existsByName(String aName);
+}
