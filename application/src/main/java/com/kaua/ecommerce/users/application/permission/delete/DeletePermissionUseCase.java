b4d8501a8443543b04fa0e004516755e74a99524
Kauã Pereira <zkauacodes@hotmail.com>
2023-09-30 17:21:31 -0300

    feat(delete-permission-usecase): add the delete permission usecase abstraction class

---
+++ application/src/main/java/com/kaua/ecommerce/users/application/permission/delete/DeletePermissionUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.users.application.permission.delete;
+
+import com.kaua.ecommerce.users.application.UnitUseCase;
+
+public abstract class DeletePermissionUseCase extends UnitUseCase<DeletePermissionCommand> {
+}
