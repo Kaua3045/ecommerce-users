49b532832e3567b8e5bccaa66dd46cbef8a616ff
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-01 11:43:22 -0300

    feat(get-permission-by-id-usecase): add the get permission by id usecase abstraction class

---
+++ application/src/main/java/com/kaua/ecommerce/users/application/permission/retrieve/get/GetPermissionByIdUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.ecommerce.users.application.permission.retrieve.get;
+
+import com.kaua.ecommerce.users.application.UseCase;
+
+public abstract class GetPermissionByIdUseCase extends
+        UseCase<GetPermissionByIdCommand, GetPermissionByIdOutput> {
+}
