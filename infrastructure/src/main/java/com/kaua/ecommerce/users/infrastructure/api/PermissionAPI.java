2961d2174aa17369c179c25a9a6859f790eb894d
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-01 12:00:20 -0300

    feat(permission-api): add the getPermission route method in interface

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/PermissionAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/api/PermissionAPI.java
@@ -1,6 +1,7 @@
 package com.kaua.ecommerce.users.infrastructure.api;
 
 import com.kaua.ecommerce.users.infrastructure.permissions.models.CreatePermissionApiInput;
+import com.kaua.ecommerce.users.infrastructure.permissions.models.GetPermissionResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,12 @@ public interface PermissionAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<?> createPermission(@RequestBody CreatePermissionApiInput input);
+
+    @GetMapping(
+            value = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    GetPermissionResponse getPermission(@PathVariable String id);
 
     @DeleteMapping("{id}")
     ResponseEntity<?> deletePermission(@PathVariable String id);
