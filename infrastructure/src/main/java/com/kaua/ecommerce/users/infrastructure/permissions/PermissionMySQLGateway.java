c82decaf5f485475fab551614fb386e21c88c6f2
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-03 20:12:24 -0300

    feat(permission-gateway): add the findAll method implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/permissions/PermissionMySQLGateway.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/permissions/PermissionMySQLGateway.java
@@ -1,9 +1,15 @@
 package com.kaua.ecommerce.users.infrastructure.permissions;
 
 import com.kaua.ecommerce.users.application.gateways.PermissionGateway;
+import com.kaua.ecommerce.users.domain.pagination.Pagination;
+import com.kaua.ecommerce.users.domain.pagination.SearchQuery;
 import com.kaua.ecommerce.users.domain.permissions.Permission;
 import com.kaua.ecommerce.users.infrastructure.permissions.persistence.PermissionJpaEntity;
 import com.kaua.ecommerce.users.infrastructure.permissions.persistence.PermissionJpaRepository;
+import com.kaua.ecommerce.users.infrastructure.utils.SpecificationUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -31,6 +37,30 @@ public class PermissionMySQLGateway implements PermissionGateway {
     @Override
     public Optional<Permission> findById(String aId) {
         return this.permissionRepository.findById(aId).map(PermissionJpaEntity::toDomain);
+    }
+
+    @Override
+    public Pagination<Permission> findAll(SearchQuery aQuery) {
+        final var aPage = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var aSpecification = Optional.ofNullable(aQuery.terms())
+                .filter(term -> !term.isBlank())
+                .map(term -> SpecificationUtils.<PermissionJpaEntity>like("name", term)
+                        .or(SpecificationUtils.like("description", term))).orElse(null);
+
+        final var aPageResult = this.permissionRepository.findAll(aSpecification, aPage);
+
+        return new Pagination<>(
+                aPageResult.getNumber(),
+                aPageResult.getSize(),
+                aPageResult.getTotalPages(),
+                aPageResult.getTotalElements(),
+                aPageResult.map(PermissionJpaEntity::toDomain).toList()
+        );
     }
 
     @Override
