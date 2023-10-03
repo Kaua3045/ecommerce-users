af073d2d3938b162236bd40586054a12a1c329c9
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-03 20:08:56 -0300

    feat(permission-jpa-repository): add the custom findAll method in jpa repository

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/permissions/persistence/PermissionJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/permissions/persistence/PermissionJpaRepository.java
@@ -1,8 +1,13 @@
 package com.kaua.ecommerce.users.infrastructure.permissions.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionJpaRepository extends JpaRepository<PermissionJpaEntity, String> {
 
     boolean existsByName(String name);
+
+    Page<PermissionJpaEntity> findAll(Specification<PermissionJpaEntity> whereClause, Pageable page);
 }
