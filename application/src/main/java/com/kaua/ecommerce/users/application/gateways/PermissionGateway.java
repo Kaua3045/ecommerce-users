5f933ba4e2a6c232654f9e85dd52c7d8a5166822
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-03 19:59:09 -0300

    feat(permission-gateway): add the findAll method in permission gateway

--- application/src/main/java/com/kaua/ecommerce/users/application/gateways/PermissionGateway.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/gateways/PermissionGateway.java
@@ -1,5 +1,7 @@
 package com.kaua.ecommerce.users.application.gateways;
 
+import com.kaua.ecommerce.users.domain.pagination.Pagination;
+import com.kaua.ecommerce.users.domain.pagination.SearchQuery;
 import com.kaua.ecommerce.users.domain.permissions.Permission;
 
 import java.util.Optional;
@@ -11,6 +13,8 @@ public interface PermissionGateway {
     boolean existsByName(String aName);
 
     Optional<Permission> findById(String aId);
+
+    Pagination<Permission> findAll(SearchQuery aQuery);
 
     Permission update(Permission aPermission);
 
