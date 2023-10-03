e51ad4d729e4aa5534e39b9bb816a5187e78a5de
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-03 16:44:34 -0300

    feat(update-permission-usecase): add update permission usecase abstraction class

---
+++ application/src/main/java/com/kaua/ecommerce/users/application/permission/update/UpdatePermissionUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.users.application.permission.update;
+
+import com.kaua.ecommerce.users.application.UseCase;
+
+public abstract class UpdatePermissionUseCase extends UseCase<UpdatePermissionCommand, UpdatePermissionOutput> {
+}
