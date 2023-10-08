206b1c6cf79020457ac5631bc0bd230fa8f52a02
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-08 08:58:07 -0300

    feat(get-role-by-id-output): add the set permissions

--- application/src/main/java/com/kaua/ecommerce/users/application/role/retrieve/get/GetRoleByIdOutput.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/role/retrieve/get/GetRoleByIdOutput.java
@@ -1,6 +1,10 @@
 package com.kaua.ecommerce.users.application.role.retrieve.get;
 
 import com.kaua.ecommerce.users.domain.roles.Role;
+import com.kaua.ecommerce.users.domain.roles.RolePermission;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record GetRoleByIdOutput(
         String id,
@@ -9,7 +13,8 @@ public record GetRoleByIdOutput(
         String roleType,
         boolean isDefault,
         String createdAt,
-        String updatedAt
+        String updatedAt,
+        Set<String> permissions
 ) {
 
     public static GetRoleByIdOutput from(final Role aRole) {
@@ -20,7 +25,11 @@ public record GetRoleByIdOutput(
                 aRole.getRoleType().name(),
                 aRole.isDefault(),
                 aRole.getCreatedAt().toString(),
-                aRole.getUpdatedAt().toString()
+                aRole.getUpdatedAt().toString(),
+                aRole.getPermissions()
+                        .stream()
+                        .map(RolePermission::getPermissionName)
+                        .collect(Collectors.toSet())
         );
     }
 }
