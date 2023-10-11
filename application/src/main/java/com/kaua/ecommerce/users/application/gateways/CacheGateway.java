534cc9a3ddbea159a5fd47286424cac5ae39410e
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-11 19:20:08 -0300

    fix(cache-gateway): fix get method, change return to Optional<T>

--- application/src/main/java/com/kaua/ecommerce/users/application/gateways/CacheGateway.java
+++ application/src/main/java/com/kaua/ecommerce/users/application/gateways/CacheGateway.java
@@ -1,8 +1,10 @@
 package com.kaua.ecommerce.users.application.gateways;
 
+import java.util.Optional;
+
 public interface CacheGateway<T> {
 
     void save(String key, T value);
-    T get(String key);
+    Optional<T> get(String key);
     void delete(String key);
 }
