8487758bc9901d84ca380c7dde460519e78edd8e
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-12 09:32:22 -0300

    test(cache-test-configuration): add the redis server embedded test configuration

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/CacheTestConfiguration.java
@@ -0,0 +1,27 @@
+package com.kaua.ecommerce.users;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+@TestConfiguration
+public class CacheTestConfiguration {
+
+    private RedisServer redisServer;
+
+    public CacheTestConfiguration(RedisProperties redisProperties) {
+        this.redisServer = new RedisServer(redisProperties.getPort());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
