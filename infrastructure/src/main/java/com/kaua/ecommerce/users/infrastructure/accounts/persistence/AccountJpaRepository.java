72d7fc8d9aa38f04c8d5e8e7276b6306ba7e5b79
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-14 17:00:55 -0300

    feat(account-jpa-repository): add the findAllWhereRoleId method

--- infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/accounts/persistence/AccountJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/users/infrastructure/accounts/persistence/AccountJpaRepository.java
@@ -1,12 +1,18 @@
 package com.kaua.ecommerce.users.infrastructure.accounts.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface AccountJpaRepository extends JpaRepository<AccountJpaEntity, String> {
 
     boolean existsByEmail(String email);
 
     Optional<AccountJpaEntity> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM accounts WHERE role_id = :roleId", nativeQuery = true)
+    Set<AccountJpaEntity> findAllWhereRoleId(@Param("roleId") String roleId);
 }
