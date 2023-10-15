ae19a8e56bdd4df0f320abfd1b38e7a40398331e
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-15 15:44:01 -0300

    refactor(cache-test-configuration): refactor all make cache tests

--- infrastructure/src/test/java/com/kaua/ecommerce/users/config/CacheTestConfiguration.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/config/CacheTestConfiguration.java
@@ -9,8 +9,8 @@ public abstract class CacheTestConfiguration {
     private static RedisServer redisServer;
 
     public void init() {
-//        final var port = RandomGenerator.getDefault().nextInt(1000, 9999);
-        redisServer = new RedisServer(7070);
+        final var port = RandomGenerator.getDefault().nextInt(1000, 9999);
+        redisServer = new RedisServer(port);
         redisServer.start();
     }
 
