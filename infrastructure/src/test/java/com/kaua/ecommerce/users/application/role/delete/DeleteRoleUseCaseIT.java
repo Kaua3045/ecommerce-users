c0658c001ff44adf41a2f489d1bfe829754410ac
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-15 17:21:34 -0300

    refactor(cache-test-configuration): refactor all make cache tests

--- infrastructure/src/test/java/com/kaua/ecommerce/users/application/role/delete/DeleteRoleUseCaseIT.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/users/application/role/delete/DeleteRoleUseCaseIT.java
@@ -3,13 +3,13 @@ package com.kaua.ecommerce.users.application.role.delete;
 import com.kaua.ecommerce.users.CacheGatewayTest;
 import com.kaua.ecommerce.users.application.usecases.role.delete.DeleteRoleCommand;
 import com.kaua.ecommerce.users.application.usecases.role.delete.DeleteRoleUseCase;
-import com.kaua.ecommerce.users.config.CacheTestConfiguration;
 import com.kaua.ecommerce.users.domain.roles.Role;
 import com.kaua.ecommerce.users.domain.roles.RoleID;
 import com.kaua.ecommerce.users.domain.roles.RoleTypes;
 import com.kaua.ecommerce.users.infrastructure.roles.persistence.RoleJpaEntity;
 import com.kaua.ecommerce.users.infrastructure.roles.persistence.RoleJpaRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -21,7 +21,7 @@ import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @CacheGatewayTest
-public class DeleteRoleUseCaseIT extends CacheTestConfiguration {
+public class DeleteRoleUseCaseIT {
 
     @Container
     private static final GenericContainer<?> redis = new GenericContainer<>(
