fac6bfaf09a7d2cb2b21f6d520f2658026ff9922
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-17 13:50:53 -0300

    feat(role-permission): add the generated annotation to exclude coverage for equals and hashcode methods

--- domain/src/main/java/com/kaua/ecommerce/users/domain/roles/RolePermission.java
+++ domain/src/main/java/com/kaua/ecommerce/users/domain/roles/RolePermission.java
@@ -2,6 +2,9 @@ package com.kaua.ecommerce.users.domain.roles;
 
 import com.kaua.ecommerce.users.domain.ValueObject;
 import com.kaua.ecommerce.users.domain.permissions.PermissionID;
+import com.kaua.ecommerce.users.domain.utils.Generated;
+
+import java.util.Objects;
 
 public class RolePermission extends ValueObject {
 
@@ -26,5 +29,20 @@ public class RolePermission extends ValueObject {
 
     public String getPermissionName() {
         return permissionName;
+    }
+
+    @Generated
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final RolePermission that = (RolePermission) o;
+        return Objects.equals(getPermissionID(), that.getPermissionID()) && Objects.equals(getPermissionName(), that.getPermissionName());
+    }
+
+    @Generated
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPermissionID(), getPermissionName());
     }
 }
